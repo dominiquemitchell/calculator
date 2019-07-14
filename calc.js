9356fcaa6b4ed38e9c3b5fcc2fff46b409cfedc5
Dominique Mitchell <amara_musicmaker@yahoo.com>
2019-07-14 13:09:21 -0400

    initial commit for the calculator

---
+++ calc.js
@@ -0,0 +1,123 @@
+// //Results display 
+// let display = document.querySelector('p').innerHTML = "0";
+// let runningTotal = 0;
+
+// //Clear 
+// // const clearButton = document.querySelector('.clearEvent');
+// // clearButton.addEventListener('click', function() {
+// //   display = document.querySelector('p').innerHTML = "0";
+// // });
+
+// //Back 
+
+// //Division
+// const divisionButton = document.querySelector('.divisionEvent'); 
+// divisionButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML = "0";
+//   runningTotal + "/";
+//   //code to divide integers
+// });
+
+// //7 Button
+// const sevenButton = document.querySelector('.sevenEvent');
+// sevenButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "7";
+//   runningTotal += 7;
+// });
+
+// //8 Button
+// const eightButton = document.querySelector('.eightEvent');
+// eightButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "8";
+//   runningTotal += 8;
+// });
+
+// //9 Button
+// const nineButton = document.querySelector('.nineEvent');
+// nineButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "9";
+//   runningTotal += 9;
+// });
+
+// //Multiply
+// const multiplyButton = document.querySelector('.multiplyEvent'); 
+// multiplyButton.addEventListener('click', function() {
+//   // document.querySelector('p').innerHTML = "0";
+//   runningTotal + "*";
+//   //code to multiply integers
+// });
+
+// //4 Button
+// const fourButton = document.querySelector('.fourEvent');
+// fourButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "4";
+//   runningTotal += 4;
+// });
+
+// //5 Button
+// const fiveButton = document.querySelector('.fiveEvent');
+// fiveButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "5";
+//   runningTotal += 5;
+// });
+
+// //6 Button
+// const sixButton = document.querySelector('.sixEvent');
+// sixButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "6";
+//   runningTotal += 6;
+// });
+
+// //Subtract
+// const subtractButton = document.querySelector('.subtractEvent'); 
+// subtractButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML = "0";
+//   runningTotal + "-";
+// });
+
+// //1 Button
+// const oneButton = document.querySelector('.oneEvent'); 
+// oneButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "1";
+//   runningTotal += 1;
+// });
+
+// //2 Button
+// const twoButton = document.querySelector('.twoEvent'); 
+// twoButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "2";
+//   runningTotal += 2;
+// });
+
+// //3 Button
+// const threeButton = document.querySelector('.threeEvent'); 
+// threeButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "3";
+//   runningTotal += 3;
+// });
+
+// //Addition
+// const addButton = document.querySelector('.addEvent'); 
+// addButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML = "0";
+
+//   console.log(runningTotal);
+// });
+
+
+
+// //0 Button
+// const zeroButton = document.querySelector('.zeroEvent'); 
+// zeroButton.addEventListener('click', function() {
+//   document.querySelector('p').innerHTML += "0";
+//   runningTotal += 0;
+// });
+
+// //Equals
+// const equalsButton = document.querySelector('.equalsEvent'); 
+// equalsButton.addEventListener('click', function() {
+//   // console.log(runningTotal);
+//   // parseInt(runningTotal);
+//   // eval(runningTotal);
+//   document.querySelector('p').innerHTML = runningTotal;
+// });
